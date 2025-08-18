49e38f60a0051df286ac3f7eeec92f3b98b1004b
Ethan Ortecho <ethan.ortecho@gmail.com>
2025-08-17 22:56:03 -0400

    heh

--- frontend/studi-app/babel.config.js
+++ frontend/studi-app/babel.config.js
@@ -1,10 +1,11 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
     plugins: [
-      // NativeWind needs this; order is fine here.
-      'nativewind/babel',
       'react-native-reanimated/plugin',
     ],
   };
